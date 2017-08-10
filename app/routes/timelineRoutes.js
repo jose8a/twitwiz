8d66fb09d294fc17a9c1e7e5e793d52b3cf331a7
jose8a <ochovilla@gmail.com>
2017-08-09 20:36:06 -0700

    Add endpoints for '/user' endpoint based on since_id, max_tweets, and since-days-past.

--- app/routes/timelineRoutes.js
+++ app/routes/timelineRoutes.js
@@ -1,3 +1,5 @@
+var Timeline = require('../helpers/timeline-helper');
+
 module.exports = function(router) {
   // Get the logged-in user's home timeline
   router.get('/', (req, res, next) => {
@@ -12,4 +14,44 @@ module.exports = function(router) {
     res.status(200).send(`I see timeline for ${listname}`);
   });
 
-}
+  // Get user timeline
+  // This method can only return up to 3,200 of a user’s most recent Tweets.
+  router.get('/user/mt/:maxtweets', (req, res, next) => {
+    // create a new userTimeline object
+    var uT = Object.create(Timeline);
+    uT.init({username: 'jose8a', type: 'MAXTWEETS', value: req.params.maxtweets});
+    uT.fetch.endpoint = 'statuses/user_timeline';
+
+    // request from Twitter API endpoint
+    uT.fetchTimeline(uT, uT.notMaxTweets, res);
+    // --- uT.fetchTLMaxTweets(req, res, next);
+  });
+
+  router.get('/user/md/:maxdays', (req, res, next) => {
+    // create and initialize a new userTimeline object
+    var uT = Object.create(Timeline);
+    uT.init({username: 'jose8a', type: 'MAXDAYS', value: req.params.maxdays});
+    uT.fetch.endpoint = 'statuses/user_timeline';
+
+    // request from Twitter API endpoint
+    uT.fetchTimeline(uT, uT.notMaxDays, res);
+    // --- uT.fetchTLMaxDays(req, res, next);
+  });
+
+  router.get('/user/sid/:sinceid', (req, res, next) => {
+    // create a new userTimeline object
+    var uT = Object.create(Timeline);
+    uT.init({username: 'jose8a', type: 'SINCEID', value: req.params.sinceid});
+    uT.fetch.endpoint = 'statuses/user_timeline';
+
+    // request from Twitter API endpoint
+    uT.fetchTimeline(uT, uT.notSinceId, res);
+    // --- uT.fetchTLSinceId(req, res, next);
+  });
+
+  router.get('/user/test/maxdays', (req, res, next) => {
+    // create a new userTimeline object
+    var uT = Object.create(Timeline);
+    res.status(200).send(uT.isMaxDays());
+  });
+};
